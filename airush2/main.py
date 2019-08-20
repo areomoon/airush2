69617acad49dcb7b21d524408f5e610e574972c2
Areomoon <tsengyangyu@gmail.com>
2019-08-20 15:27:25 +0800

    change parameters

--- airush2/main.py
+++ airush2/main.py
@@ -29,67 +29,6 @@ import nsml
 # Refactor to summarize from existing experiment code.
 
 
-# weighted BCE loss function
-# def weighted_binary_cross_entropy(sigmoid_x, targets, pos_weight, weight=None, size_average=True, reduce=True):
-#     """
-#     Args:
-#         sigmoid_x: predicted probability of size [N,C], N sample and C Class. Eg. Must be in range of [0,1], i.e. Output from Sigmoid.
-#         targets: true value, one-hot-like vector of size [N,C]
-#         pos_weight: Weight for postive sample
-#     """
-#     if not (targets.size() == sigmoid_x.size()):
-#         raise ValueError("Target size ({}) must be the same as input size ({})".format(targets.size(), sigmoid_x.size()))
-#
-#     loss = -pos_weight * targets * sigmoid_x.log() - (1-targets)*(1-sigmoid_x).log()
-#
-#     if weight is not None:
-#         loss = loss * weight
-#
-#     if not reduce:
-#         return loss
-#     elif size_average:
-#         return loss.mean()
-#     else:
-#         return loss.sum()
-#
-# class WeightedBCELoss(nn.Module):
-#     def __init__(self, pos_weight=1, weight=None, PosWeightIsDynamic= False, WeightIsDynamic= False, size_average=True, reduce=True):
-#         """
-#         Args:
-#             pos_weight = Weight for postive samples. Size [1,C]
-#             weight = Weight for Each class. Size [1,C]
-#             PosWeightIsDynamic: If True, the pos_weight is computed on each batch. If pos_weight is None, then it remains None.
-#             WeightIsDynamic: If True, the weight is computed on each batch. If weight is None, then it remains None.
-#         """
-#         super().__init__()
-#
-#         self.register_buffer('weight', weight)
-#         self.register_buffer('pos_weight', pos_weight)
-#         self.size_average = size_average
-#         self.reduce = reduce
-#         self.PosWeightIsDynamic = PosWeightIsDynamic
-#
-#     def forward(self, input, target):
-#         # pos_weight = Variable(self.pos_weight) if not isinstance(self.pos_weight, Variable) else self.pos_weight
-#         if self.PosWeightIsDynamic:
-#             positive_counts = target.sum(dim=0)
-#             nBatch = len(target)
-#             self.pos_weight = (nBatch - positive_counts)/(positive_counts +1e-5)
-#
-#         if self.weight is not None:
-#             # weight = Variable(self.weight) if not isinstance(self.weight, Variable) else self.weight
-#             return weighted_binary_cross_entropy(input, target,
-#                                                  self.pos_weight,
-#                                                  weight=self.weight,
-#                                                  size_average=self.size_average,
-#                                                  reduce=self.reduce)
-#         else:
-#             return weighted_binary_cross_entropy(input, target,
-#                                                  self.pos_weight,
-#                                                  weight=None,
-#                                                  size_average=self.size_average,
-#                                                  reduce=self.reduce)
-
 if not nsml.IS_ON_NSML:
     DATASET_PATH = os.path.join('/home/kwpark_mk2/airush2_temp')
     DATASET_NAME = 'airush2_temp'
@@ -323,18 +262,18 @@ def main(args):
             if epoch % args.save_epoch_every == 0:
                 nsml.save('epoch_' + str(epoch))  # this will save your current model on nsml.
 
-            for i, data in enumerate(valid_loader):
-                images, extracted_image_features, labels, flat_features = data
-
-                images = images.cuda()
-                extracted_image_features = extracted_image_features.cuda()
-                flat_features = flat_features.cuda()
-                labels = labels.cuda()
-                # forward
-                if args.arch == 'MLP':
-                    logits = model(extracted_image_features, flat_features)
-                elif args.arch == 'Resnet':
-                    logits = model(images, flat_features)
+            # for i, data in enumerate(valid_loader):
+            #     images, extracted_image_features, labels, flat_features = data
+            #
+            #     images = images.cuda()
+            #     extracted_image_features = extracted_image_features.cuda()
+            #     flat_features = flat_features.cuda()
+            #     labels = labels.cuda()
+            #     # forward
+            #     if args.arch == 'MLP':
+            #         logits = model(extracted_image_features, flat_features)
+            #     elif args.arch == 'Resnet':
+            #         logits = model(images, flat_features)
 
     nsml.save('final')
 
@@ -360,7 +299,7 @@ if __name__ == '__main__':
     parser.add_argument('--lr', type=float, default=1e-3)
     parser.add_argument('--print_every', type=int, default=10)
     parser.add_argument('--save_epoch_every', type=int, default=2)
-    parser.add_argument('--save_step_every', type=int, default=1000)
+    parser.add_argument('--save_step_every', type=int, default=300)
 
     parser.add_argument('--use_gpu', type=bool, default=True)
     parser.add_argument("--arch", type=str, default="MLP")
